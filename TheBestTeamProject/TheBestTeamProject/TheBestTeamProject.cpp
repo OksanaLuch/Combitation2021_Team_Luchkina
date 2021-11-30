4b09c916a1b576750abdb538bc2e8aa245e6a1d1
OksanaLuch <luchkina.oksanka@mail.ru>
2021-11-30 19:50:20 +0300

    Добавлены исходные данные

--- TheBestTeamProject/TheBestTeamProject/TheBestTeamProject.cpp
+++ TheBestTeamProject/TheBestTeamProject/TheBestTeamProject.cpp
@@ -1,21 +1,47 @@
-﻿// TheBestTeamProject.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include "pch.h"
+﻿#include "pch.h"
+#include <string>
 #include <iostream>
+
+
+
+//vremya raboty algoritma, kakoy massiv
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	int VhodDannie1[500], VhodDannie2[500], VhodDannie3[500], VhodDannie4[500], VhodDannie5[500];
+	int i;
+
+	for (i = 0; i <= 499; i++) {
+		VhodDannie1[i] = i;
+		std::cout << VhodDannie1[i];
+	}
+
+	for (i = 0; i <= 499; i++) {
+		VhodDannie2[i] = 1;
+		std::cout << VhodDannie2[i];
+	} 
+
+	for (i = 0; i <= 499; i++) {
+		if (i>=0 && i<100) VhodDannie3[i] = 1;
+		if (i >= 100 && i < 200) VhodDannie3[i] = 2;
+		if (i >= 200 && i < 300) VhodDannie3[i] = 3;
+		if (i >= 300 && i < 400) VhodDannie3[i] = 4;
+		if (i >= 400 && i < 500) VhodDannie3[i] = 5;
+		std::cout << VhodDannie3[i] + '  ';
+	}
+
+	for (i = 0; i <= 499; i++) {
+		VhodDannie4[i] = -1000 + rand() % (0 -1000*(-1) + 1);
+		std::cout << VhodDannie4[i];
+	}
+
+	for (i = 0; i <= 499; i++) {
+		VhodDannie5[i] = 1000000*(-1) + rand() % (0 - 1000000 * (-1) + 1);
+		std::cout << VhodDannie5[i];
+	}
+
+	return 0;
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
